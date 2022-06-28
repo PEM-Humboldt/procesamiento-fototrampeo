1101e9d336277707ae8fe74f294a342597a0da69
Marcelo Villa <marcelo.villa.p@gmail.com>
2022-06-27 21:31:36 -0500

    .

---
+++ src/create_dwc_archive.py
@@ -0,0 +1,23 @@
+"""
+Genera las siguientes tablas para posteriores análisis:
+
+- Event Core
+- Occurrence Core
+- Measurement or Fact Extension
+- Simple Multimedia Extension
+"""
+import pathlib
+import warnings
+
+import click
+import regi0
+import wiutils
+
+
+@click.command()
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    warnings.filterwarnings("ignore")
